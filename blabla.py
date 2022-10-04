fbafcac72d530e873142c75c91f911e7d21b4879
Sander <sander.jonkers+github@gmail.com>
2022-10-04 21:14:50 +0200

    Create blabla.py

---
+++ blabla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+
+def check_docker():
+    # Trick 1: if in docker, /proc/1/cgroup contains docker
+    DOCKER = False
+    try:
+        with open("/proc/1/cgroup", "rt") as ifh:
+            DOCKER = ":/docker/" in ifh.read()
+    except:
+        pass
+
+    DOCKER = os.path.isfile("/.dockerenv")
+
+
+    # Trick 2: if in docker and privileged docker, then /proc/sys/vm/swappiness is writable
+    DOCKER_PRIVILEGED = False
+    if DOCKER:
+        try:
+            f = open("/proc/sys/vm/swappiness", "a")
+            f.close()
+            print("writable!")
+            DOCKER_PRIVILEGED = True
+        except:
+            DOCKER_PRIVILEGED = False
+            print("not writable")
+            # sys.exit(0)
+
+    return DOCKER, DOCKER_PRIVILEGED
+
+if __name__ == '__main__':
+
+    print(check_docker())
+    
